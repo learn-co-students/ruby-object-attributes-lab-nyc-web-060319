2f518e0a5169912142970fdc7201664346703cc6
github username <github email address>
2019-06-16 22:59:57 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+
+#defines the variable "name" to the instance variable @name
+def name
+    @name
+end
+
+#takes in an argument of name that's assigned to the instance variable of @dog
+def name=(name)
+    @name = name
+end
+
+def breed
+    @breed
+end
+
+def breed=(breed)
+    @breed = breed
+end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+class Person
+
+def person
+    @Person
+end
+
+def name
+    @name
+end
+
+def name=(name)
+    @name = name
+end
+
+def job
+    @job
+end
+
+def job=(job)
+    @job = job
+end
+
+end
